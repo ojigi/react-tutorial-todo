f8fc6b2c0220df09775385aa18aba1660fdbcb4d
taku_mizumoto <taku.amaryllis@gmail.com>
2020-08-18 17:52:44 +0900

    Updates

---
+++ precache-manifest.27f8de8954b0e5dc9608053a5ec4b369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05dc7126e9eeee219b2ec824f1da8bf8",
+    "url": "/react-tutorial-todo/index.html"
+  },
+  {
+    "revision": "5a947f7bd3ccb81d696c",
+    "url": "/react-tutorial-todo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ec10a2947dd094616dd3",
+    "url": "/react-tutorial-todo/static/js/2.38da934b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial-todo/static/js/2.38da934b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a947f7bd3ccb81d696c",
+    "url": "/react-tutorial-todo/static/js/main.e69dedd1.chunk.js"
+  },
+  {
+    "revision": "613149b1f5304623c5ca",
+    "url": "/react-tutorial-todo/static/js/runtime-main.c27daaba.js"
+  }
+]);
